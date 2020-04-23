e3e4546fed5e45aefe6cb70979671b510730c8a8
Robert Kaye <rob@musicbrainz.org>
2020-04-23 15:28:33 +0200

    Fix another user name

--- admin/sql/create_db.sql
+++ admin/sql/create_db.sql
@@ -1,5 +1,5 @@
 -- Create the user and the database. Must run as user postgres.
 
 CREATE USER messybrainz NOCREATEDB NOSUPERUSER;
-ALTER USER listenbrainz WITH PASSWORD 'messybrainz';
+ALTER USER messybrainz WITH PASSWORD 'messybrainz';
 CREATE DATABASE messybrainz WITH OWNER = messybrainz TEMPLATE template0 ENCODING = 'UNICODE';
